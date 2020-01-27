a8ebcd5f19f324339c7981fcb5dd8e0f65564898
Shinchul Park <shpark@c4r4s7.42seoul.kr>
2020-01-28 03:00:57 +0900

    go

---
+++ c01/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "========== PSC's MOULINETTE =========="
+
+echo "========== MOVE FILE =========="
+
+for i in $(seq 0 8); do
+	cd ex0$i/
+	mv main.c ../../../ex0$i/
+done
+
+cd ../../
+
+echo "========== NORMINETTE  =========="
+
+norminette -R CheckForbiddenSourceHeader | cat
+
+echo "---------- GCC"
+
+for i in $(seq 1 8); do
+	cd ex0$i/
+	echo "========== COMPILE =========="
+	gcc -Wall -Wextra -Werror main.c *.c | cat
+	chmod +x a.out
+	echo "========== RESULT =========="
+	./a.out
+	rm -rf a.out
+done
